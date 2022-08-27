74f508d66004cd9442b8ed055803c720a862e9e3
Pierrick Turelier <pierrick@turelier.com>
2022-08-26 19:20:20 -0500

    feat(interfaces): add EIP5164 interfaces

---
+++ src/interfaces/ICrossChainReceiver.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.16;
+
+import "./ICrossChainRelayer.sol";
+
+/**
+ * @title CrossChainReceiver interface
+ * @notice CrossChainReceiver interface of the ERC5164 standard as defined in the EIP.
+ */
+interface ICrossChainReceiver {
+  /**
+   * @notice
+   * @param target Address that will be called
+   * @param data Data that will be sent to the `target` address
+   * @param value Gas value needed to execute the call
+   */
+  struct Call {
+    address target;
+    bytes data;
+    uint256 value;
+  }
+
+  /**
+   * @notice Emitted when calls have successfully been received.
+   * @param relayer Address of the contract that relayed the calls
+   * @param nonce Unique identifier
+   * @param caller Address of the bridge transport layer
+   * @param calls Array of calls being received
+   */
+  event ReceivedCalls(
+    ICrossChainRelayer indexed relayer,
+    uint256 indexed nonce,
+    address indexed caller,
+    Call[] calls
+  );
+
+  /**
+   * @notice Receive calls from the origin chain.
+   * @dev Should authenticate the `caller` as being the bridge transport layer.
+   * @dev Must emit the `ReceivedCalls` event when calls are received.
+   * @param relayer Address who relayed the call on the origin chain
+   * @param nonce Unique identifier
+   * @param caller Address of the bridge transport layer
+   * @param calls Array of calls being received
+   */
+  function receiveCalls(
+    ICrossChainRelayer relayer,
+    uint256 nonce,
+    address caller,
+    Call[] calldata calls
+  ) external;
+}
---
+++ src/interfaces/ICrossChainRelayer.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.16;
+
+import "./ICrossChainReceiver.sol";
+
+/**
+ * @title CrossChainRelayer interface
+ * @notice CrossChainRelayer interface of the ERC5164 standard as defined in the EIP.
+ */
+interface ICrossChainRelayer {
+  /**
+   * @notice
+   * @param target Address that will be called on the receiving chain
+   * @param data Data that will be sent to the `target` address
+   * @param value Gas value needed to execute the call on the receiving chain
+   */
+  struct Call {
+    address target;
+    bytes data;
+    uint256 value;
+  }
+
+  /**
+   * @notice Emitted when calls have successfully been relayed to the receiver chain.
+   * @param nonce Unique identifier
+   * @param sender Address of the sender
+   * @param receiver Address of the CrossChainReceiver contract on the receiving chain
+   * @param calls Array of calls being relayed
+   * @param gas Amount of gas required for the `calls` to be executed
+   */
+  event RelayedCalls(
+    uint256 indexed nonce,
+    address indexed sender,
+    ICrossChainReceiver indexed receiver,
+    Call[] calls,
+    uint256 gas
+  );
+
+  /**
+   * @notice Relay the calls to the receiving chain.
+   * @dev Must implement `ICrossChainReceiver.receiveCalls` to relay the calls on the receiving chain.
+   * @dev Must increment a `nonce` so that each batch of calls can be uniquely identified.
+   * @dev Must emit the `RelayedCalls` event when successfully called.
+   * @dev May require payment. Some bridges may require payment in the native currency, so the function is payable.
+   * @param receiver Address who will receive the calls on the receiving chain
+   * @param calls Array of calls being relayed
+   * @param gas Amount of gas required for the `calls` to be executed
+   */
+  function relayCalls(
+    ICrossChainReceiver receiver,
+    Call[] calldata calls,
+    uint256 gas
+  ) external payable;
+}
